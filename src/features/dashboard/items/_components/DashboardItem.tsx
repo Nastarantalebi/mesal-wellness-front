02a57bd57b5d927a05b4c819d76e570a323729c8
front1-mesal <front1.mesal@gmail.com>
2025-11-23 15:17:32 +0330

    fix bugs

--- src/features/dashboard/items/_components/DashboardItem.tsx
+++ src/features/dashboard/items/_components/DashboardItem.tsx
@@ -1,18 +1,30 @@
 import useGetData from "@/services/useGetData";
 import type { TDashboard } from "../_types/type";
 import StatisticsCard from "../../layout/StatisticsCard";
+import CustomerTable from "../../layout/CustomerTable";
 
 const DashboardItem = () => {
   const { data } = useGetData<TDashboard>({
     url: "basics/dashboard/getData",
     queryKey: "DASHBOARD_QUERY_KEY",
   });
-
+  const tableKeys = ["رزروهای اخیر", "مشتریان اخیر"];
   return (
-    <div>
+    <div className="space-y-6">
       {data?.widgets.tiles && (
         <StatisticsCard titleText="آمار وب‌سایت" items={data.widgets.tiles} />
       )}
+
+      {data?.widgets.lists &&
+        tableKeys.map((key) => {
+          const listData = data.widgets.lists[key];
+          return listData ? (
+            <CustomerTable
+              key={key}
+              listData={listData}
+            />
+          ) : null;
+        })}
     </div>
   );
 };
---
+++ src/features/dashboard/layout/CustomerTable.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import Table from "@/components/Table";
+import type { TDashboard } from "../items/_types/type";
+
+interface TProps {
+  listData?: TDashboard["widgets"]["lists"][string];
+}
+
+const CustomerTable: React.FC<TProps> = ({ listData }) => {
+  const [orders, setOrders] = useState(listData?.data || []);
+
+  useEffect(() => {
+    setOrders(listData?.data || []);
+  }, [listData]);
+
+  if (!orders.length) {
+    return <div>داده‌ای وجود ندارد.</div>;
+  }
+
+  return (
+    <div className="col-span-12 mt-5">
+      <div className="text-lg font-medium mb-3">
+        {"customer_name" in orders[0] ? "رزروهای اخیر" : "مشتریان اخیر"}
+      </div>
+      <div className="overflow-auto">
+        <Table className="border-spacing-y-[10px] border-separate w-full">
+          <Table.Tbody>
+            {orders.map((order) => (
+              <Table.Tr key={order.id}>
+                <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                  <div className="flex flex-col">
+                    <div className="font-medium">
+                      {"customer_name" in order
+                        ? order.customer_name
+                        : order.full_name}
+                    </div>
+                    <div className="text-xs text-slate-500">{order.phone}</div>
+                  </div>
+                </Table.Td>
+                <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                  <div className="text-xs text-slate-500">کد ملی</div>
+                  <div>{order.national_code || "-"}</div>
+                </Table.Td>
+
+                {"customer_name" in order ? (
+                  <>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">تاریخ رزرو</div>
+                      <div className="whitespace-nowrap">
+                        {order.start_at.split(" ")[0]}
+                      </div>
+                    </Table.Td>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">ساعت</div>
+                      <div className="whitespace-nowrap">
+                        {order.end_at.split(" ")[1]} -{" "}
+                        {order.start_at.split(" ")[1]}
+                      </div>
+                    </Table.Td>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">وضعیت</div>
+                      <div>{order.status}</div>
+                    </Table.Td>
+                  </>
+                ) : (
+                  <>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">جنسیت</div>
+                      <div>{order.gender}</div>
+                    </Table.Td>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">نوع عضویت</div>
+                      <div>{order.membership_type}</div>
+                    </Table.Td>
+                    <Table.Td className="box shadow-[5px_3px_5px_#00000005] dark:bg-darkmode-600">
+                      <div className="text-xs text-slate-500">تاریخ عضویت</div>
+                      <div>{order.joined_at || "-"}</div>
+                    </Table.Td>
+                  </>
+                )}
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerTable;
