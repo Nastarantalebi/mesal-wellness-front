3a594a6260920f85aa2760fd98135fe85b231df2
front1-mesal <front1.mesal@gmail.com>
2025-11-25 12:39:32 +0330

    fix bugs

--- src/features/dashboard/layout/Topbar.tsx
+++ src/features/dashboard/layout/Topbar.tsx
@@ -30,7 +30,6 @@ function Topbar({
     const el = document.documentElement;
     if (el.requestFullscreen) el.requestFullscreen();
   };
-
   return (
     <div className="fixed top-0 inset-x-0 z-10 h-[65px] box bg-slate-50 border-x-0 border-t-0 rounded-none flex shadow-none">
       <div
@@ -45,21 +44,17 @@ function Topbar({
         onMouseLeave={(event) => {
           event.preventDefault();
           setCompactMenuOnHover(false);
-        }}
-      >
+        }}>
         <a
           href=""
-          className="hidden xl:flex items-center transition-[margin] group-[.side-menu--collapsed]:xl:ms-2 group-[.side-menu--collapsed.side-menu--on-hover]:xl:ms-0"
-        >
+          className="hidden xl:flex items-center transition-[margin] group-[.side-menu--collapsed]:xl:ms-2 group-[.side-menu--collapsed.side-menu--on-hover]:xl:ms-0">
           <div className="flex items-center justify-center w-[34px] rounded-lg h-[34px]">
-            <img src="/logo-192×192.png" alt="لوگو"/>
+            <img src="/logo-192×192.png" alt="لوگو" />
           </div>
-        
         </a>
         <button
           onClick={toggleCompactMenu}
-          className="group-[.side-menu--collapsed.side-menu--on-hover]:xl:opacity-100 group-[.side-menu--collapsed]:xl:rotate-180 group-[.side-menu--collapsed]:xl:opacity-0 transition-[opacity,transform] hidden 3xl:flex items-center justify-center w-[20px] h-[20px] ms-auto border rounded-full border-slate-600/40 hover:bg-slate-600/5"
-        >
+          className="group-[.side-menu--collapsed.side-menu--on-hover]:xl:opacity-100 group-[.side-menu--collapsed]:xl:rotate-180 group-[.side-menu--collapsed]:xl:opacity-0 transition-[opacity,transform] hidden 3xl:flex items-center justify-center w-[20px] h-[20px] ms-auto border rounded-full border-slate-600/40 hover:bg-slate-600/5">
           <Lucide icon="ArrowLeft" className="w-3.5 h-3.5 stroke-[1.3]" />
         </button>
         <div className="flex items-center gap-1 xl:hidden">
@@ -69,8 +64,7 @@ function Topbar({
               event.preventDefault();
               setActiveMobileMenu(true);
             }}
-            className="p-2 rounded-full hover:bg-slate-100"
-          >
+            className="p-2 rounded-full hover:bg-slate-100">
             <AlignJustify className="w-[18px] h-[18px]" />
           </a>
           <a
@@ -79,8 +73,7 @@ function Topbar({
             onClick={(e) => {
               e.preventDefault();
               setQuickSearch(true);
-            }}
-          >
+            }}>
             <Lucide icon="Search" className="w-[18px] h-[18px]" />
           </a>
         </div>
@@ -94,12 +87,10 @@ function Topbar({
           {/* BEGIN: Search */}
           <div
             className="relative justify-center flex-1 hidden xl:flex"
-            onClick={() => setQuickSearch(true)}
-          >
+            onClick={() => setQuickSearch(true)}>
             <div className="bg-slate-100 border w-[350px] flex items-center py-2 px-3.5 rounded-[0.5rem] text-slate-400 cursor-pointer hover:bg-slate-50 transition-colors">
               <Lucide icon="Search" className="w-[18px] h-[18px]" />
               <div className="ms-2.5 me-auto">جستجوی سریع...</div>
-              <div>⌘K</div>
             </div>
           </div>
           <QuickSearch
@@ -116,8 +107,7 @@ function Topbar({
                 onClick={(e) => {
                   e.preventDefault();
                   setActivitiesPanel(true);
-                }}
-              >
+                }}>
                 <Lucide icon="LayoutGrid" className="w-[18px] h-[18px]" />
               </a>
               {/* <a href="" className="p-2 rounded-full hover:bg-slate-100">
@@ -129,8 +119,7 @@ function Topbar({
                 onClick={(e) => {
                   e.preventDefault();
                   requestFullscreen();
-                }}
-              >
+                }}>
                 <Lucide icon="Expand" className="w-[18px] h-[18px]" />
               </a>
               <a
@@ -139,8 +128,7 @@ function Topbar({
                 onClick={(e) => {
                   e.preventDefault();
                   setNotificationsPanel(true);
-                }}
-              >
+                }}>
                 <Lucide icon="Bell" className="w-[18px] h-[18px]" />
               </a>
             </div>
@@ -150,55 +138,50 @@ function Topbar({
                   alt="تیل وایز - قالب داشبورد مدیریتی"
                   src={users.fakeUsers()[0].photo}
                 /> */}
+                 <Lucide icon="User" className="w-5 h-5 mx-auto" />
               </Menu.Button>
               <Menu.Items className="w-56 mt-1">
                 <Menu.Item
                   onClick={() => {
-                    setSwitchAccount(true);
-                  }}
-                >
-                  <Lucide icon="ToggleLeft" className="w-4 h-4 me-2" />
-                  تغییر حساب
+                    navigate("settings");
+                  }}>
+                  <Lucide icon="User" className="w-4 h-4 me-2" />
+                 نام کاربر
                 </Menu.Item>
                 <Menu.Divider />
                 <Menu.Item
                   onClick={() => {
                     navigate("settings?page=connected-services");
-                  }}
-                >
+                  }}>
                   <Lucide icon="Settings" className="w-4 h-4 me-2" />
                   خدمات متصل
                 </Menu.Item>
                 <Menu.Item
                   onClick={() => {
                     navigate("settings?page=email-settings");
-                  }}
-                >
+                  }}>
                   <Lucide icon="Inbox" className="w-4 h-4 me-2" />
                   تنظیمات ایمیل
                 </Menu.Item>
                 <Menu.Item
                   onClick={() => {
                     navigate("settings?page=security");
-                  }}
-                >
+                  }}>
                   <Lucide icon="Lock" className="w-4 h-4 me-2" />
                   بازنشانی رمز عبور
                 </Menu.Item>
                 <Menu.Divider />
                 <Menu.Item
                   onClick={() => {
-                    navigate("settings");
-                  }}
-                >
-                  <Lucide icon="Users" className="w-4 h-4 me-2" />
-                  اطلاعات پروفایل
+                    setSwitchAccount(true);
+                  }}>
+                  <Lucide icon="ToggleLeft" className="w-4 h-4 me-2" />
+                  تغییر حساب
                 </Menu.Item>
                 <Menu.Item
                   onClick={() => {
                     navigate("login");
-                  }}
-                >
+                  }}>
                   <Lucide icon="Power" className="w-4 h-4 me-2" />
                   خروج
                 </Menu.Item>
