6bdb86600e09f49cee0ef31ccd8b497e516ae64c
front1-mesal <front1.mesal@gmail.com>
2025-12-06 15:53:59 +0330

    fix bugs

---
+++ src/components/Tabulator/TableHeader.tsx
@@ -0,0 +1,50 @@
+import Button from "../Button";
+import Lucide from "../Lucide";
+type TProps = {
+  addText: string;
+  title: string;
+  customAdd: (() => void) | undefined;
+  onAdd: (() => void) | undefined;
+  customAddText?: string;
+};
+const TableHeader = ({
+  addText,
+  title,
+  customAdd,
+  onAdd,
+  customAddText,
+}: TProps) => {
+  return (
+    <div className="flex flex-row items-center justify-between gap-y-2">
+      <div className="text-base font-medium group-[.mode--light]:text-white">
+        {title}
+      </div>
+      <div className="flex flex-col sm:flex-row gap-x-3 gap-y-2 md:ms-auto">
+        {customAdd && (
+          <Button
+            onClick={customAdd}
+            variant="outline-primary"
+            className="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent">
+            <Lucide
+              icon="CalendarCheck2"
+              className="stroke-[1.3] w-4 h-4 me-2"
+            />{" "}
+            {customAddText}
+          </Button>
+        )}
+        {onAdd && (
+          <Button
+            onClick={onAdd}
+            variant="outline-success"
+            className="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent">
+            <Lucide icon="Plus" className="stroke-[1.3] w-4 h-4 me-2" />{" "}
+            <span className="md:hidden inline-block"> جدید</span>
+            <span className="hidden md:inline-block">{addText}</span>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TableHeader;
--- src/components/Tabulator/index.tsx
+++ src/components/Tabulator/index.tsx
@@ -4,8 +4,6 @@ import { TabulatorFull as Tabulator } from "tabulator-tables";
 import { createIcons, icons } from "lucide";
 import { mapFieldsToColumns } from "./columnsTransform";
 import type { TColumns, TPaginate } from "../../types";
-import Button from "../Button";
-import Lucide from "../Lucide";
 import { createRoot } from "react-dom/client";
 import TitlePage from "@/features/_components/TitlePage";
 import Pagination from "./Pagination";
@@ -15,6 +13,7 @@ import type {
 } from "@tanstack/react-query";
 import ImportData from "./ImportData";
 import ActionsColumns from "./ActionsColumns";
+import TableHeader from "./TableHeader";
 export type TableAction = {
   title: string;
   icon: React.ReactNode;
@@ -149,35 +148,13 @@ function CustomTable({
     <div className="grid grid-cols-12 gap-y-10 gap-x-6">
       <TitlePage title={title} />
       <div className="col-span-12">
-        <div className="flex flex-row items-center justify-between gap-y-2">
-          <div className="text-base font-medium group-[.mode--light]:text-white">
-            {title}
-          </div>
-          <div className="flex flex-col sm:flex-row gap-x-3 gap-y-2 md:ms-auto">
-            {customAdd && (
-              <Button
-                onClick={customAdd}
-                variant="outline-primary"
-                className="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent">
-                <Lucide
-                  icon="CalendarCheck2"
-                  className="stroke-[1.3] w-4 h-4 me-2"
-                />{" "}
-                {customAddText}
-              </Button>
-            )}
-            {onAdd && (
-              <Button
-                onClick={onAdd}
-                variant="outline-success"
-                className="group-[.mode--light]:!bg-white/[0.12] group-[.mode--light]:!text-slate-200 group-[.mode--light]:!border-transparent">
-                <Lucide icon="Plus" className="stroke-[1.3] w-4 h-4 me-2" />{" "}
-                <span className="md:hidden inline-block"> جدید</span>
-                <span className="hidden md:inline-block">{addText}</span>
-              </Button>
-            )}
-          </div>
-        </div>
+        <TableHeader
+          addText={addText}
+          customAdd={customAdd}
+          customAddText={customAddText}
+          onAdd={onAdd}
+          title={title}
+        />
         <div className="flex flex-col gap-8 mt-3.5">
           <div className="flex flex-col box box--stacked">
             <ImportData
