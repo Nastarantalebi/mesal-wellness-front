50c6564ab3ed77e0182135d97d03f8cdb3e65bd7
front1-mesal <front1.mesal@gmail.com>
2025-12-17 13:47:47 +0330

    fix bugs

--- src/features/basics/contract/_fixtures/data.ts
+++ src/features/basics/contract/_fixtures/data.ts
@@ -4,12 +4,22 @@ import type { TReqContract } from "../_types/types";
 export const url = "/basics/contracts/";
 export const queryKey = "contractQuerykey";
 
-export const schema = z.object({
-  finished_at: z.string().min(1, " "),
-  start_at: z.string().min(1, " "),
-  discount_percent: z.string(),
-  company_id: z.coerce.number().min(1, " "),
-});
+export const schema = z
+  .object({
+    finished_at: z.string().min(1, " "),
+    start_at: z.string().min(1, " "),
+    discount_percent: z.string(),
+    company_id: z.coerce.number().min(1, " "),
+  })
+  .refine(
+    (data) => {
+      return new Date(data.finished_at) > new Date(data.start_at);
+    },
+    {
+      message: "تاریخ پایان باید بعد از تاریخ شروع باشد",
+      path: ["finished_at"],
+    }
+  );
 
 export const initialValue: TReqContract = {
   discount_percent: "0",
