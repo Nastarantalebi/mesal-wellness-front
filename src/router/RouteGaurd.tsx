012102ab09afb78c05c097cf667a1f2f8eb9a546
front1-mesal <front1.mesal@gmail.com>
2026-01-27 18:37:44 +0330

    fix bugs

--- src/router/RouteGaurd.tsx
+++ src/router/RouteGaurd.tsx
@@ -4,17 +4,16 @@ import { useAuthStore } from "@/features/auth/store/authStore";
 import { useEffect, type JSX } from "react";
 import Forbidden from "@/features/_components/Forbidden";
 import { plainInstance } from "@/libs/axios";
-import { useOtpStore } from "@/features/auth/store/otpState";
 interface TProps {
   children: JSX.Element;
 }
 
 const RouteGuard = ({ children }: TProps) => {
   const navigate = useNavigate();
-  const mobile = useOtpStore((s) => s.mobile);
   const location = useLocation();
   const sidebar = useAuthStore((s) => s.sidebar?.menus);
   const auth = useAuthStore((s) => s.auth);
+  const mobile = useAuthStore((s) => s.userData?.user.mobile);
   const clearAuth = useAuthStore((s) => s.clearAuth);
   if (!auth) return <Navigate to="/login" replace />;
   if (auth && !auth.organizations.length)
