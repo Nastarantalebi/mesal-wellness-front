a724090cd3f75b3aab355dbde840fa99ee047198
front1-mesal <front1.mesal@gmail.com>
2026-02-04 10:57:53 +0330

    pie chart

--- src/features/dashboard/items/_components/widget/PieCahrtWidget.tsx
+++ src/features/dashboard/items/_components/widget/PieCahrtWidget.tsx
@@ -15,13 +15,16 @@ const PieCahrtWidget = ({ data }: Props) => {
       {data.map((chart, index) => (
         <div
           key={index}
-          className="col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-xl p-4 shadow">
-          <h3 className="mb-4 font-semibold text-gray-700">{chart.title}</h3>
+          className="col-span-12 md:col-span-6 lg:col-span-4 bg-white rounded-xl shadow">
+          <h3 className="mb-4 font-semibold text-gray-700 py-2 px-4">
+            {chart.title}
+          </h3>
 
-          <div className="w-full h-[250px]">
+          <div className="w-full h-[300px]">
             <ResponsiveContainer>
               <PieChart>
                 <Pie
+                  stroke="none"
                   data={chart.data.values.map((item) => ({
                     ...item,
                     fill: item.color,
@@ -30,7 +33,7 @@ const PieCahrtWidget = ({ data }: Props) => {
                   nameKey="label"
                   cx="50%"
                   cy="50%"
-                  outerRadius={80}
+                  outerRadius="100%"
                 />
 
                 <Tooltip
@@ -50,6 +53,23 @@ const PieCahrtWidget = ({ data }: Props) => {
                   verticalAlign="middle"
                   align="right"
                   layout="vertical"
+                  className="text-right"
+                  content={({ payload }: any) => {
+                    console.log(payload);
+                    return (
+                      <ul className="flex flex-col gap-2 text-sm">
+                        {payload.map((entry: any, index: number) => (
+                          <li key={index} className="flex items-center gap-2">
+                            <span
+                              className="w-3 h-3 rounded-full"
+                              style={{ backgroundColor: entry.color }}
+                            />
+                            <span className="text-gray-600">{entry.value}</span>
+                          </li>
+                        ))}
+                      </ul>
+                    );
+                  }}
                 />
               </PieChart>
             </ResponsiveContainer>
