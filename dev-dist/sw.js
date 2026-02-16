076cfda217b4d2ea6312a872f6d8dcf9e28d67fb
front1-mesal <front1.mesal@gmail.com>
2026-02-16 11:43:01 +0330

    fix bugs

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oskfg61m12o"
+    "revision": "0.ckm356qlomo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
