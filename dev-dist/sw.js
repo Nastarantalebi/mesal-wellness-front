f421500087449c0c710eeb10de7c40710e349939
nastaran <nastarantalebi.1382@gmail.com>
2026-03-16 12:02:55 +0330

    add text to about section[deploy]

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vv8uspv41e8"
+    "revision": "0.nik079cnp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
