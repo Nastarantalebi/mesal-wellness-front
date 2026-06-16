b0ab55d2a3a5f7860c2da2b0a6c6248275347714
talebi <nastarantalebi.1382@gmail.com>
2026-06-16 11:48:44 +0330

    fix(api)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.v6ggnieg9ns"
+    "revision": "0.7de5po0dtbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
